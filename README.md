acc4fdfbff728dd0d49d5dc78d1ee2078b35ee0e
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-27 08:46:05 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # HuggingFacePlay
 ### 试用抱抱脸AI模型库
 
+<details><summary>语言情感积极性分析</summary>
+<img src="./效果/语言情感积极性分析.png"/>
+</details>
+
+<details><summary>文章前提问答</summary>
+<img src="./效果/文章前提问答.png"/>
+</details>
+
 环境：python3.11.3、TensorFlow、transformers、 FastAPI、Pycharm
 
 说明：使用 FastAPI库生成一个各种人工智能接口API测试页面
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import uvicorn
 sentimentAnalysis = pipeline('sentiment-analysis')
 #根据正文信息回答问题
 question_answerer = pipeline('question-answering')
+#文本生成
+textGenerator = pipeline("text-generation")
+
 app = FastAPI(title="抱抱脸接口测试")
 
 @app.get("/sentiment", summary='情绪分析', tags=['文本相关'])
@@ -19,6 +22,11 @@ def qa(text: str = None):
 def qa(text: str = None, q_text: str = None):
     result = question_answerer({'question': q_text, 'context': text})
     result = result['answer'] # 解析结果，只要需要的
+    return {"code": 200, "result": result}
+
+@app.get("/tg", summary='文本生成', tags=['文本相关'])
+def qa(text: str = None):
+    result = textGenerator(text)
     return {"code": 200, "result": result}
 
 if __name__ == '__main__':
