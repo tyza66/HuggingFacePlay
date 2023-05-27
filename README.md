ff303127afbf989bd1e553685c1a07bf136d3731
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-27 08:20:43 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HuggingFacePlay
-试用抱抱脸AI模型库
+### 试用抱抱脸AI模型库
+
+环境：python3.11.3、TensorFlow、transformers、 FastAPI、Pycharm
+
+说明：使用 FastAPI库生成一个各种人工智能接口API测试页面
+
+访问地址：http://127.0.0.1:8000/docs
+
+By：tyza66(洮羱芝闇)
---
+++ main.py
@@ -0,0 +1,25 @@
+from transformers import pipeline
+from fastapi import FastAPI
+import uvicorn
+
+#引用各种模型
+#语言是否积极
+sentimentAnalysis = pipeline('sentiment-analysis')
+#根据正文信息回答问题
+question_answerer = pipeline('question-answering')
+app = FastAPI(title="抱抱脸接口测试")
+
+@app.get("/sentiment", summary='情绪分析', tags=['文本相关'])
+def qa(text: str = None):
+    result = sentimentAnalysis(text)
+    result = result[0]['label'] # 解析结果，只要需要的
+    return {"code": 200, "result": result}
+
+@app.get("/qa", summary='文本问答', tags=['文本相关'])
+def qa(text: str = None, q_text: str = None):
+    result = question_answerer({'question': q_text, 'context': text})
+    result = result['answer'] # 解析结果，只要需要的
+    return {"code": 200, "result": result}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=8000)
